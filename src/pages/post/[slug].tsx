e5ed90c93f859593b1a5af0010bf6cfd8b99f265
SitaGomes <ansitagomes@gmail.com>
2021-11-29 15:08:37 +0100

    Add: getStaticPaths, getStaticProps on post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import Prismic from "@prismicio/client"
+
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -26,20 +28,52 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({post}: PostProps) {
+  return(
+    <h1>Post</h1>
+  )
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(
+    Prismic.Predicates.at("document.type", "post")
+  );
 
-//   // TODO
-// };
+  const slugs = posts.results.map(post => {
+    return {
+      uid: post.uid
+    }
+  })
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths: [
+      {params: {slug: slugs.find(slug => slug.uid === "como-utilizar-hooks").uid}},
+      {params: {slug: slugs.find(slug => slug.uid === "criando-um-app-cra-do-zero").uid}},
+    ],
+    fallback: true
+  }
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async context => {
+
+  const {slug} = context.params;
+
+  const prismic = getPrismicClient();
+  const response: Post = await prismic.getByUID("post", String(slug), {});
+
+  const post: Post = {
+    first_publication_date: response.first_publication_date,
+    data: {
+      author: response.data.author,
+      banner: response.data.banner,
+      content: response.data.content,
+      title: response.data.title,
+    }
+  }
+
+  return { 
+    revalidate: 60 * 60 * 12,
+    props: {post}
+  }
+};
